39b963823d5b902cbb2956b750b510bc9c479d87
wynn3312 <104gojib2@naver.com>
2026-06-06 16:58:53 +0900

    [feature-kyeongsu-authentication][김경수] Django 설정 업데이트: JWT 토큰 발급을 위한 rest_framework_simplejwt 추가 및 CORS 설정 강화

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -22,6 +23,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'api',
+    'rest_framework_simplejwt', # jwt 토큰 발급 앱 추가 
 ]
 
 MIDDLEWARE = [
@@ -91,3 +93,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_CREDENTIALS = True
+
+SESSION_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_HTTPONLY = True
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
